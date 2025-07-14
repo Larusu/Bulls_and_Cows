aa04029ec025e4f4d5a923e938b9694990c26a21
Larusu <ldaneliee33@gmail.com>
2025-07-14 21:05:47 +0800

    added setup on bulls and cows file

---
+++ BullsAndCows/BullsAndCowsGame.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+struct GameSetting
+{
+    int length = 4;
+    int maxGuesses = 7;
+    int difficulty = 1; // 1 easy, 2 medium, 3 hard
+};
+
+struct GameState
+{ // Tracking the progress and storing each secret code
+    string playerSecretCode;
+    string aiSecretCode;
+    int turn = 1;
+};
+
+void setupGame(GameSetting& settings, GameState& state);
+void printMenu(int length);
+string generateUniqueDigitCode(int length);
+
+int main()
+{
+    srand(time(0));
+
+    GameSetting settings;
+    GameState state;
+    char choice;
+
+    printMenu(settings.length);
+    do
+    {
+        setupGame(settings, state);
+        cout << "Play another game? (y/n): ";
+        cin >> choice; cin.ignore();
+    } while (tolower(choice) == 'y');
+}
+
+void setupGame(GameSetting& settings, GameState& state)
+{ // Changing game settings here
+    
+    
+    while (settings.difficulty != 1 && settings.difficulty != 2)
+    {
+        cout << "Choose the difficulty of the game (1 for easy, 2 for medium): ";
+        cin >> settings.difficulty;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid input. Choose only 1 (Easy) or 2 (Medium). \n";
+            continue;
+        }
+        cin.ignore();
+
+        if (settings.difficulty < 1 || settings.difficulty > 2)
+        {
+            cout << "Choose only 1 or 2";
+        }
+    }
+
+    // Generating Secret Code
+    state.aiSecretCode = generateUniqueDigitCode(settings.length);
+    state.playerSecretCode = getPlayerSecretCode(settings.length);
+
+    switch (settings.difficulty)
+    {
+    case 1: startEasyGame(settings, state); break;
+    case 2: startMediumGame(settings, state); break;
+    }
+}
+
+string generateUniqueDigitCode(int length)
+{
+    string RandomCode = "";
+
+    while (RandomCode.length() < length)
+    {
+        char random = (rand() % 10) + '0';
+
+        if (RandomCode.find(random) == string::npos)
+        {
+            RandomCode += random;
+        }
+    }
+    return RandomCode;
+}
+
+void printMenu(int length)
+{
+    cout << endl;
+    cout << "==========================================" << endl;
+    cout << "        Welcome to Bulls and Cows!" << endl;
+    cout << "==========================================" << endl;
+    cout << "Rules: " << endl;
+    cout << "1. You need to choose the difficulty of the game (Easy, Medium). " << endl;
+    cout << "2. You and the AI will each guess each other secret code." << endl;
+    cout << "3. Each digit must be unique and between 0 and 9." << endl;
+    cout << "4. You will try to guess the AI's code, and the AI will try to guess yours." << endl;
+    cout << "5. After each guess, you'll be told how many Bulls and Cows you got:" << endl;
+    cout << "   - Bulls: Correct digit in the correct position." << endl;
+    cout << "   - Cows:  Correct digit in the wrong position." << endl;
+    cout << "6. Each side has a maximum of " << game.maxGuesses << " guesses." << endl;
+    cout << "   - If neither side guesses the correct answer within " << game.maxGuesses << " attempts," << endl;
+    cout << "     the game ends in draw." << endl;
+    cout << "Let's start!" << endl << endl;
+}
